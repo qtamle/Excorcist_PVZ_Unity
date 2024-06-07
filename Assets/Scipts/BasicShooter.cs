46f62a4feb7d826acece8b05cb3c00329b469706
EyelessJack0903 <eyelessjack0903@gmail.com>
2024-06-07 21:14:11 +0700

    update fire

--- Assets/Scipts/BasicShooter.cs
+++ Assets/Scipts/BasicShooter.cs
@@ -8,8 +8,8 @@ public class BasicShooter : MonoBehaviour
     public Transform shootOrigin;
     public float cooldown;
 
-    private bool canShoot = true;
-    private bool isReady = false;
+    private bool canShoot;
+
     public float range;
 
     public LayerMask shootMask;
@@ -18,34 +18,38 @@ public class BasicShooter : MonoBehaviour
 
     private Animator animator;
 
+    private bool IsCoorect = true;
     private void Start()
     {
+        Invoke("ResetCooldown", cooldown);
         animator = GetComponent<Animator>();
     }
 
+    
     private void Update()
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, range, shootMask);
+        
         if (hit.collider)
         {
             target = hit.collider.gameObject;
-            if (!isReady)
-            {
+                Stop (false);
                 ReadyShot(true);
-                isReady = true;
-            }
-            SetShootAnimation(true);
-            Shoot();
+                if (target != null)
+                {
+                    Idle(false);
+                    SetShootAnimation(true);
+                    Shoot();
+                }
         }
         else
         {
-            if (isReady)
-            {
-                Stop(true);
-                isReady = false;
-            }
+            Stop(true);
+            Idle(true);
         }
+        
     }
+    
 
     void ResetCooldown()
     {
@@ -56,10 +60,9 @@ public class BasicShooter : MonoBehaviour
     {
         if (!canShoot)
             return;
-
         canShoot = false;
         Invoke("ResetCooldown", cooldown);
-        Instantiate(bullet, shootOrigin.position, Quaternion.identity);
+        GameObject myBullet = Instantiate(bullet, shootOrigin.position, Quaternion.identity);
     }
 
     void SetShootAnimation(bool isShooting)
@@ -70,7 +73,7 @@ public class BasicShooter : MonoBehaviour
         }
     }
 
-    void ReadyShot(bool ready)
+    void ReadyShot (bool ready)
     {
         if (animator != null)
         {
@@ -78,29 +81,25 @@ public class BasicShooter : MonoBehaviour
         }
     }
 
-    void Stop(bool stop)
+    void Stop (bool stop)
     {
-        if (animator != null)
+        if(animator != null)
         {
             animator.SetBool("stop", stop);
-            if (stop)
-            {
-                Idle(true);
-                ReadyShot(false);
-                SetShootAnimation(false);
-            }
-            else
-            {
-                Idle(false);
-            }
         }
     }
 
-    void Idle(bool idle)
+    void Idle (bool idle)
     {
         if (animator != null)
         {
             animator.SetBool("idle", idle);
+            if (idle)
+            {
+                ReadyShot(false);
+                SetShootAnimation(false);
+            }
         }
     }
+
 }
