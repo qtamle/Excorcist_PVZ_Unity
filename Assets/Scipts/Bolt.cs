dce0acc15cec230e032c12b2c89eb55b5a6a9ab4
EyelessJack0903 <eyelessjack0903@gmail.com>
2024-06-10 20:03:04 +0700

    update wizard lighting

---
+++ Assets/Scipts/Bolt.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bolt : MonoBehaviour
+{
+    public int damage;
+    public float explosionRadius = 2f; // Bán kính của vụ nổ
+    public float speed = 0.8f;
+    public bool freeze;
+
+    private void Update()
+    {
+        transform.position += new Vector3(speed * Time.fixedDeltaTime, 0, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.TryGetComponent<Zombie>(out Zombie zombie))
+        {
+            Explode(zombie); // Kích hoạt hàm vụ nổ khi bắn trúng zombie
+            Destroy(gameObject);
+        }
+    }
+
+    void Explode(Zombie target)
+    {
+        // Tìm tất cả các zombie trong bán kính nổ
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, explosionRadius);
+
+        // Duyệt qua từng zombie trong bán kính nổ và gây sát thương cho chúng
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.TryGetComponent<Zombie>(out Zombie zombie))
+            {
+                zombie.Hit(damage, freeze);
+            }
+        }
+    }
+}
---
+++ Assets/Scipts/CoconutCanon.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoconutCanon : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawnPoint;
+    public float cooldownTime = 5f; // Thời gian cooldown
+    private bool canShoot = true; // Biến để kiểm tra có thể bắn hay không
+    public LayerMask targetLayer; // LayerMask cho đối tượng mà đạn sẽ va chạm và bị hủy
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && canShoot)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.collider != null && hit.collider.gameObject == gameObject)
+            {
+                Shoot();
+                StartCoroutine(Cooldown());
+            }
+        }
+    }
+
+    IEnumerator Cooldown()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(cooldownTime);
+        canShoot = true;
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity);
+        bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(10f, 0f); // Bắn đạn về phía bên phải
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == targetLayer)
+        {
+            Destroy(collision.gameObject); // Hủy đối tượng khi va chạm với LayerMask được chỉ định
+        }
+    }
+}
